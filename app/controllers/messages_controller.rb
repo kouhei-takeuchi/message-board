45c8dca035418ac85b6ef1d99657541c8e5e1c4a
kouhei.takeuchi <skywalker.ko1809@gmail.com>
2019-11-14 12:16:10 +0000

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,25 +1,56 @@
 class MessagesController < ApplicationController
-    def index
-        @messages = Message.all
+  before_action :set_message, only: [:show, :edit, :ipdate, :destroy]
+  
+  def index
+    @messages = Message.all
+  end
+
+  def show
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.new(message_params)
+
+    if @message.save
+      flash[:success] = 'Message が正常に投稿されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message が投稿されませんでした'
+      render :new
     end
-    
-    def show
-        @message = Message.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @message.update(message_params)
+      flash[:success] = 'Message は正常に更新されました'
+      redirect_to @message
+    else
+      flash.now[:danger] = 'Message は更新されませんでした'
+      render :edit
     end
-    
-    def new
-        @message = Message.new
-    end
-    
-    def create
-    end
-    
-    def edit
-    end
-    
-    def update
-    end
-    
-    def destroy
-    end
+  end
+
+  def destroy
+    @message.destroy
+
+    flash[:success] = 'Message は正常に削除されました'
+    redirect_to messages_url
+  end
+
+  private
+
+  def set_message
+    @message = Message.find(params[:id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
 end
